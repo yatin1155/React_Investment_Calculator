8e18f2c3bc68d681a08d34d200d85ac03c3b2f9c
LAPTOP-TD2QPNFN\HP <yatinkapur@gmail.com>
2024-05-26 01:34:12 +0530

    Added core logic for grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import UserInput from "./components/UserInput"
+import Results from "./components/Results"
+
+const DEFAULT_USER_INPUT = {
+  initialInvestment: 10000,
+  annualInvestment: 1200,
+  expectedReturn: 6,
+  duration: 10
+}
 
 function App() {
+  const [userInput, setUserInput] = useState(DEFAULT_USER_INPUT)
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput(prevUserState => {
+      return {
+        ...prevUserState,
+        [inputIdentifier]: +newValue
+      }
+    })
+
+  }
   return (
     <>
       <Header></Header>
-      <UserInput></UserInput>
+      <UserInput inputValues={userInput} onUpdate={handleChange}></UserInput>
+      <Results inputValues={userInput}></Results>
     </>
   )
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,26 +1,28 @@
 
-export default function UserInput() {
+export default function UserInput({ inputValues, onUpdate }) {
+
+
     return (
         <section id="user-input">
             <div className="input-group">
                 <p>
                     <label>Initial Investment</label>
-                    <input type="number" required />
+                    <input onChange={(event) => onUpdate('initialInvestment', event.target.value)} value={inputValues.initialInvestment} type="number" required />
                 </p>
                 <p>
                     <label>Annual Investment</label>
-                    <input type="number" required />
+                    <input onChange={(event) => onUpdate('annualInvestment', event.target.value)} value={inputValues.annualInvestment} type="number" required />
                 </p>
 
             </div>
             <div className="input-group">
                 <p>
                     <label>Expected Return</label>
-                    <input type="number" required />
+                    <input onChange={(event) => onUpdate('expectedReturn', event.target.value)} value={inputValues.expectedReturn} type="number" required />
                 </p>
                 <p>
                     <label>Duration</label>
-                    <input type="number" required />
+                    <input onChange={(event) => onUpdate('duration', event.target.value)} value={inputValues.duration} type="number" required />
                 </p>
 
             </div>
